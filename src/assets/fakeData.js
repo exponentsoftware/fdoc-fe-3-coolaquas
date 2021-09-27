c685de729b11f36a0ebc7de1f0417a6213fb4f99
coolaquas <samrat.mallick1@gmail.com>
2021-09-27 18:57:58 +0530

    solved

---
+++ src/assets/fakeData.js
@@ -0,0 +1,65 @@
+export const fakeData = [
+  {
+    id: 1,
+    album_title: "Rockstar123",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+  {
+    id: 2,
+    album_title: "Rockstar",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+  {
+    id: 3,
+    album_title: "Rockstar",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+  {
+    id: 4,
+    album_title: "Rockstar",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+  {
+    id: 5,
+    album_title: "Rockstar",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+  {
+    id: 6,
+    album_title: "Rockstar",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+  {
+    id: 7,
+    album_title: "Rockstar",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+  {
+    id: 8,
+    album_title: "Rockstar",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+  {
+    id: 9,
+    album_title: "Rockstar",
+    artist: "A.R.Rahaman",
+    album_cover:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
+  },
+];
